b8e5453fe9cab24a5d9f4a1209448549b009b129
ugamezeRepo <ugamezeRepo@gmail.com>
2024-02-14 13:18:25 +0900

    완성인가요?? 일단 기능은 댄다...

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import submitImg from '../assets/arrow-up.svg';
 import { Input } from 'antd';
 import axios from 'axios';
@@ -8,39 +8,36 @@ const { TextArea } = Input;
 //axios.defaults.baseURL = 'http://localhost:9191/tmpgpt/api/rooms';
 axios.defaults.baseURL = 'http://192.168.0.148:9191/tmpgpt/api/rooms';
 
-
 const ChatInput = (props) => {
   const [inputText, setInputText] = useState('');
   const navigate = useNavigate();
 
- const haveProps= async()=>{
-  let id = 0;
+  const haveProps = async () => {
+    let id = 0;
 
-  if (Object.keys(props).length === 0) {
-    console.log(Object.keys(props).length);
-    await axios.post('', {
-      roomName: inputText
-    });
-    await axios.get('/last').then((res) => {
-      id = res.data
-    });
-    await axios.post('/'+id+'/chat', {
-      msg:inputText,
-      writer:"me",
-      roomId:id
-    })
-    navigate('/chat/' + id);
-   }else{
-    props.post(inputText);
-    setInputText('');
-   }
-  }
-
+    if (Object.keys(props).length === 0) {
+      console.log(Object.keys(props).length);
+      await axios.post('', {
+        roomName: inputText,
+      });
+      await axios.get('/last').then((res) => {
+        id = res.data;
+      });
+      await axios.post('/' + id + '/chat', {
+        msg: inputText,
+        writer: 'me',
+        roomId: id,
+      });
+      navigate('/chat/' + id);
+    } else {
+      props.post(inputText);
+      setInputText('');
+    }
+  };
 
   const buttonClick = () => {
     console.log('button clicked');
     haveProps();
-
   };
 
   const pressEnter = (e) => {
@@ -60,6 +57,7 @@ const ChatInput = (props) => {
         autoSize={{
           minRows: 2,
         }}
+        autoFocus
       />
       <button className="submitButton" onClick={buttonClick}>
         <img src={submitImg} alt="" />
